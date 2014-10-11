f345ad5fb7a1fba0a30e84805f431e8a9d28dced
Jade Deng <jade@jadedeng.com>
2014-10-11 22:52:01 +0200

    incorported input NSMutableArray

---
+++ GitMelt/GitMeIt/GitMeIt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitMeIt
+//
+//  Created by Jade Deng on 2014-10-11.
+//  Copyright (c) 2014 Jade Deng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
