f420469ac8d35e269c6d1dbeb302570f953ee9ae
Ruoping Xu <r29xu@uwaterloo.ca>
2014-10-11 04:20:00 +0200

    my init commit

---
+++ gifmaker/gifmaker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gifmaker
+//
+//  Created by Ruoping Xu on 2014-10-11.
+//  Copyright (c) 2014 Ruoping Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
