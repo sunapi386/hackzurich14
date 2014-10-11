1216e39cef9b2c551786877777d777b6fb5f5e98
Jade Deng <jade@jadedeng.com>
2014-10-11 16:36:10 +0200

    Initial Commit

---
+++ GifMeIt/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GifMeIt
+//
+//  Created by Jade Deng on 2014-10-11.
+//  Copyright (c) 2014 Jade Deng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
