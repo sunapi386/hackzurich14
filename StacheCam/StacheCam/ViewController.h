e46f769c4e83d03ab7661a3143d6257600a3b292
Jade Deng <jade@jadedeng.com>
2014-10-12 03:52:08 +0200

    transition works

---
+++ StacheCam/StacheCam/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StacheCam
+//
+//  Created by Jade Deng on 2014-10-12.
+//
+//
+
+#ifndef StacheCam_ViewController_h
+#define StacheCam_ViewController_h
+
+
+#endif
---
+++ StacheCam/StacheCam/ViewController.m
@@ -0,0 +1,13 @@
+//
+//  ViewController.m
+//  StacheCam
+//
+//  Created by Jade Deng on 2014-10-12.
+//
+//
+
+#ifndef StacheCam_ViewController_m
+#define StacheCam_ViewController_m
+
+
+#endif
