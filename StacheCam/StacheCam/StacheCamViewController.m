d59b7806c458cc93596495f1c61df568467c8c29
Jason Sun <sunapi386@gmail.com>
2014-10-12 02:06:53 +0200

    used gif libs without dying

--- StacheCam/StacheCam/StacheCamViewController.m
+++ StacheCam/StacheCam/StacheCamViewController.m
@@ -30,6 +30,8 @@
 #import "StacheCamViewController+AVFFaceDetection.h"
 #import "StacheCamViewController+Graphics.h"
 #import "UserDefaults.h"
+#import "animateGIF/exportAnimatedGif.h"
+#import "animateGIF/UIImage+animatedGIF.h"
 
 static char * const AVCaptureStillImageIsCapturingStillImageContext = "AVCaptureStillImageIsCapturingStillImageContext";
 const CGFloat FACE_RECT_BORDER_WIDTH = 3;
@@ -237,6 +239,7 @@ CGFloat DegreesToRadians(CGFloat degrees) {return degrees * M_PI / 180;}
 {
     NSLog(@"Button touch up");
     self.isButtonDown = false;
+    NSURL* exportUrl = exportAnimatedGif(self.bunchOfURL);
 }
 
 
